4db7580e631a578b09c23f79e64df41f42034c3c
marceloslo <marcelosartlocatelli@gmail.com>
2023-05-26 13:17:37 -0300

    tests for dynamic statistics

---
+++ tests/DynamicStatisticsTest.java
@@ -0,0 +1,57 @@
+// #if EstatisticasDinamicas
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import urna.VotesStatisticsObserver;
+import urna.Candidate;
+import urna.StopTrap;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class DynamicStatisticsTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	public VotesStatisticsObserver v;
+
+	@BeforeEach
+	public void setUp() {
+		v = new VotesStatisticsObserver();
+	    System.setOut(new PrintStream(outContent));
+	}
+
+	@AfterEach
+	public void tearDown() {
+	    System.setOut(originalOut);
+	}
+	
+	@Test
+	void testValidVote() {
+		Candidate c = new Candidate("OdeioJava","ru",66);
+		v.update(c, "Valid");
+		assertTrue(outContent.toString().contains("66 - ru - OdeioJava - 1 - 100,00%"));
+	}
+	
+	@Test
+	void testNullVote() {
+		v.update(null, "Null");
+		assertTrue(outContent.toString().contains("Votos nulos: 1"));
+	}
+	
+	@Test
+	void testBlankVote() {
+		v.update(null, "Blank");
+		assertTrue(outContent.toString().contains("Votos brancos: 1"));
+	}
+	
+	@Test
+	void testInvalidVote() {
+		assertThrows(StopTrap.class, () -> {
+			v.update(null, "invalido");
+		});
+	}
+
+}
+// #endif
