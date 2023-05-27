ea20b598ab245c7113543b7dcaecb6901a5f9b34
Marcelo Sartori Locatelli <57831311+marceloslo@users.noreply.github.com>
2023-05-27 19:55:45 -0300

    Fix main

--- src/UrnaElection.java
+++ src/UrnaElection.java
@@ -3,7 +3,7 @@ package urna;
 public class UrnaElection {
 
 	public static void main(String[] args) {
-		Urna urnaEletronica = Urna.getInstance();
+		Urna urnaEletronica = new Urna("password");
 
 	    urnaEletronica.startMenu();
 	}
