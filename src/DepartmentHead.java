4c88ccc9398e70189aea3b5fdb847bba606aa4a2
arthurbrito99 <arthur.brito1999@outlook.com>
2023-06-04 15:53:26 -0300

    Candidato de Universidade

--- src/DepartmentHead.java
+++ src/DepartmentHead.java
@@ -1,3 +1,20 @@
-public class DepartmentHead {
-    
-}
+public class DepartmentHead extends UniversityCandidate{
+      
+    public DepartmentHead(Builder builder) {
+        super(builder);
+    }
+
+    public static class Builder extends UniversityCandidate.Builder<Builder>{
+        
+        @Override
+        public Builder getThis(){
+            return this;
+        }
+
+        public DepartmentHead build() {
+            return new DepartmentHead(this);
+        }
+
+    }    
+
+};
--- src/UniversityCandidate.java
+++ src/UniversityCandidate.java
@@ -1,3 +1,40 @@
-public class UniversityCandidate {
+package urna;
+
+public class UniversityCandidate extends Candidate {
+
+    public String district;
+
+    protected UniversityCandidate(Builder builder) {
+        super(builder);
+        this.district = builder.district;
+    }
+
+    public static abstract class Builder<T extends Builder<T>> 
+                            extends Candidate.Builder<Builder<T>> {
+
+        private String district;
+
+        public T district(String district) {
+            verifyValidDistrict(district);this.district = district;
+            return this.getThis();
+        }
+
+        public UniversityCandidate build() {
+            return new UniversityCandidate(this);
+        }
+
+        protected void verifyValidDistrict(String district){
+            
+            if (district == null)
+                throw new IllegalArgumentException("district mustn't be null");
     
+            if (district.isEmpty())
+                throw new IllegalArgumentException("district mustn't be empty");
+        }
+        
+        @Override
+        public abstract T getThis();
+
+    }
+
 }
