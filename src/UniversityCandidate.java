3869790f1c5828e8f3cc0467e592fc5e8c5e1d78
AndreDutraDCC <cienciandre@gmail.com>
2023-06-04 17:26:21 -0300

    University Candidate Implementation

--- src/UniversityCandidate.java
+++ src/UniversityCandidate.java
@@ -1,3 +1,22 @@
-public class UniversityCandidate {
-    
+package urna;
+import java.util.Set;
+
+public class UniversityCandidate extends Candidate {
+
+    public UniversityCandidate(Builder builder) {
+        super(builder);
+    }
+
+    public static class Builder extends Candidate.Builder<Builder> {
+
+        @Override
+        public Builder getThis() {
+            return this;
+        }
+        public UniversityCandidate build() {
+            return new UniversityCandidate(this);
+        }
+
+    }
+
 }
